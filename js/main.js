15423ec2e6d24c99b6a4521c3341a74a75a29ae5
Elena <olpetrushova@gmail>
2017-12-19 21:33:05 +0200

    added carousel

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+	$(".owl-carousel").owlCarousel({
+  	loop:true,
+  	items:1,
+  	//nav:true,
+  	smartSpeed: 800,
+    animateIn: 'slideInUp',
+    animateOut: 'slideOutUp',
+  })
+  
+	
+});
+   
